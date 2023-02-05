ce96a62e8a85e2a1c2ee135fb67e5e9a454d05c4
zzeng31 <zinuozeng@gmail.com>
2023-02-05 12:53:59 -0800

    stack(array)

---
+++ stack(array).js
@@ -0,0 +1,17 @@
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+  push(value) {
+    this.array.push(value);
+    return this;
+  }
+  pop() {
+    this.array.pop();
+    return this;
+  }
+}
+const myStack = new Stack();
